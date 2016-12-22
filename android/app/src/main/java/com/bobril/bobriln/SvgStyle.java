3ea2a81b573f578a8fbaa6d5d189bc8dda486dcf
Boris Letocha <boris.letocha@gmail.com>
2016-12-23 00:12:30 +0100

    Svg opacity support

--- android/app/src/main/java/com/bobril/bobriln/SvgStyle.java
+++ android/app/src/main/java/com/bobril/bobriln/SvgStyle.java
@@ -11,6 +11,8 @@ public class SvgStyle {
     int flags;
     int strokeColor;
     int fillColor;
+    float opacity;
+    float finalOpacity;
     float strokeWidth;
     Paint strokePaint;
     Paint fillPaint;
@@ -20,6 +22,7 @@ public class SvgStyle {
         strokeWidth = 0;
         strokeColor = 0;
         fillColor = 0;
+        opacity = 1;
     }
 
     public boolean setStyle(String styleName, Object styleValue) {
@@ -44,6 +47,14 @@ public class SvgStyle {
                 fillColor = ColorUtils.toColor(styleValue);
                 flags |= F_FILL_COLOR;
             }
+        } else if (styleName.equals("opacity")) {
+            if (styleValue == null) {
+                opacity = 1;
+            } else {
+                opacity = FloatUtils.parseFloat(styleValue);
+                if (opacity < 0) opacity = 0;
+                else if (opacity > 1) opacity = 1;
+            }
         } else {
             return false;
         }
@@ -52,51 +63,59 @@ public class SvgStyle {
 
     public void ReadInherited(SvgStyle merge) {
         int newFlags = ~flags;
-        if ((newFlags & F_FILL_COLOR)!=0) {
+        if ((newFlags & F_FILL_COLOR) != 0) {
             this.fillColor = merge.fillColor;
         }
-        if ((newFlags & F_STROKE_COLOR)!=0) {
+        if ((newFlags & F_STROKE_COLOR) != 0) {
             this.strokeColor = merge.strokeColor;
         }
-        if ((newFlags & F_STROKE_WIDTH)!=0) {
+        if ((newFlags & F_STROKE_WIDTH) != 0) {
             this.strokeWidth = merge.strokeWidth;
         }
+        finalOpacity = opacity * merge.finalOpacity;
     }
 
     public void AddDefaults() {
         int newFlags = ~flags;
-        if ((newFlags & F_FILL_COLOR)!=0) {
+        if ((newFlags & F_FILL_COLOR) != 0) {
             this.fillColor = Color.TRANSPARENT;
         }
-        if ((newFlags & F_STROKE_COLOR)!=0) {
+        if ((newFlags & F_STROKE_COLOR) != 0) {
             this.strokeColor = Color.TRANSPARENT;
         }
-        if ((newFlags & F_STROKE_WIDTH)!=0) {
+        if ((newFlags & F_STROKE_WIDTH) != 0) {
             this.strokeWidth = 1;
         }
+        finalOpacity = opacity;
     }
 
     public void update() {
-        if (fillColor==Color.TRANSPARENT) {
+        if (fillColor == Color.TRANSPARENT || finalOpacity == 0) {
             fillPaint = null;
         } else {
-            if (fillPaint==null) {
+            if (fillPaint == null) {
                 fillPaint = new Paint();
                 fillPaint.setAntiAlias(true);
                 fillPaint.setStyle(Paint.Style.FILL);
             }
             fillPaint.setColor(fillColor);
+            if (finalOpacity < 1) {
+                fillPaint.setAlpha((int) (fillPaint.getAlpha() * finalOpacity + 0.5));
+            }
         }
-        if (strokeColor==Color.TRANSPARENT || strokeWidth==0) {
+        if (strokeColor == Color.TRANSPARENT || strokeWidth == 0 || finalOpacity == 0) {
             strokePaint = null;
         } else {
-            if (strokePaint==null) {
+            if (strokePaint == null) {
                 strokePaint = new Paint();
                 strokePaint.setAntiAlias(true);
                 strokePaint.setStyle(Paint.Style.STROKE);
             }
             strokePaint.setColor(strokeColor);
             strokePaint.setStrokeWidth(strokeWidth);
+            if (finalOpacity < 1) {
+                strokePaint.setAlpha((int) (strokePaint.getAlpha() * finalOpacity + 0.5));
+            }
         }
     }
 }
